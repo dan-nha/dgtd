18784b5ed2c48939e10977c8759feacf368fb142
Dan-Nha Huynh <dhuynh@physik.hu-berlin.de>
2021-10-07 21:46:30 +0200

    [input] Add json file reader class

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,50 @@
 #include "dgtd_solver.h"
+#include "pde/advection.h"
 #include "spatial_solver/basis_functions/legendre_basis.h"
 #include "spatial_solver/mesh/check_mesh.h"
 #include "temporal_solver/low_storage_runge_kutta.h"
-#include "pde/advection.h"
-#include "tools/output.h"
+#include "tools/input.h"
 
-
-
+#include <boost/program_options.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 #include <iostream>
+
+namespace pt = boost::property_tree;
 
 void stream_welcome_message() {
   std::cout << "\033[31m"
-    << "---------------------------------------\n"
-    << "Welcome to DGTD \n\n"
-    << "This is a one-dimensional \n"
-    << "discontinuous Galerkin time-domain \n"
-    << "(DGTD) solver.\n"
-    << "---------------------------------------\n\033[0m"
-    << std::endl;
+            << "---------------------------------------\n"
+            << "Welcome to DGTD \n\n"
+            << "This is a one-dimensional \n"
+            << "discontinuous Galerkin time-domain \n"
+            << "(DGTD) solver.\n"
+            << "---------------------------------------\n\033[0m"
+            << std::endl;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   stream_welcome_message();
 
   Mesh::Check_mesh check_mesh(argv[1]);
-  
-  const size_t polynomial_order(20);
-  const double end_time(1);
-  //const size_t polynomial_order(3);
-  //const double end_time(0.1);
-  const double dt_factor(0.75 * 0.5);
-  const double upwind_param(0.);
 
-  DGTD::Dgtd_solver<Advection, DG::Legendre_basis, TD::Low_storage_runge_kutta>
-    dgtd(argv[1], polynomial_order, end_time, dt_factor, upwind_param);
-  Advection advection(2*M_PI);
-  dgtd.get_solution(advection, 4, 5);
+  Input input(argv[2]);
+
+  if (input.pde_name == "advection") {
+    DGTD::Dgtd_solver<
+        Advection,
+        DG::Legendre_basis,
+        TD::Low_storage_runge_kutta>
+        dgtd(
+            argv[1],
+            input.polynomial_order,
+            input.end_time,
+            input.dt_factor,
+            input.upwind_param);
+
+    Advection advection(input.advection_speed);
+    
+    dgtd.get_solution(
+        advection, input.runge_kutta_order, input.runge_kutta_stages);
+  }
 }
-
---
+++ src/tools/input.h
@@ -0,0 +1,48 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <boost/program_options.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+namespace pt = boost::property_tree;
+
+class Input {  
+  
+  public:
+    Input(const std::string& json_filename){
+      pt::ptree root;
+      pt::read_json(json_filename, root);
+
+      end_time = root.get<double>("end_time");
+      for (auto &&region_tree : root.get_child("regions")) {
+        const pt::ptree &region_params = region_tree.second;
+
+        pde_name = region_params.get<std::string>("pde");
+        polynomial_order = region_params.get<size_t>("polynomial_order");
+        runge_kutta_order = region_params.get<size_t>("runge_kutta_order");
+        runge_kutta_stages =
+        region_params.get<size_t>("runge_kutta_stages");
+        dt_factor = region_params.get<double>("dt_factor");
+        upwind_param = region_params.get<double>("upwind_param");
+
+        if (pde_name == "advection") {
+          for (auto &&param_tree : region_params.get_child("parameters")) {
+            const pt::ptree &pde_params = param_tree.second;
+            advection_speed = pde_params.get<double>("advection_speed");
+          }
+        }
+      }
+    };
+
+    std::string pde_name;
+    size_t polynomial_order;
+    size_t runge_kutta_order;
+    size_t runge_kutta_stages;
+    double dt_factor;
+    double end_time;
+    double upwind_param;
+    double advection_speed;
+};
+
+#endif
